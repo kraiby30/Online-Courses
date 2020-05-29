80772e3b12edd39a48d786d25fcc7fa93ac4a352
kraiby30 <kraiby30@gmail.com>
2020-05-28 23:46:39 -0400

    Changed print statement

--- Algorithm Toolbox/PA1/APlusB.cpp
+++ Algorithm Toolbox/PA1/APlusB.cpp
@@ -9,6 +9,6 @@ int main() {
     int b = 0;
     std::cin >> a;
     std::cin >> b;
-    std::cout << sum_of_two_digits(a, b);
+    std::cout << "Sum = "<<sum_of_two_digits(a, b)<<std::endl;
     return 0;
 }
